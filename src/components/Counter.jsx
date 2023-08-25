e0b2ea501809b528a20b0951115c6adec2936113
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-08-25 11:20:22 +0300

    couter switch

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,32 +1,72 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
+
+const initCounterValue = { countNum: 0 };
+
+function counterReducer(state, action) {
+  console.log('state ===', state);
+  console.log('action ===', action);
+
+  //   if (action === 'UP') {
+  //     return { countNum: state.countNum + 1 };
+  //   } else if (action === 'DOWN') {
+  //     return { countNum: state.countNum - 1 };
+  //   } else if (action === 'RESET') {
+  //     return { countNum: 0 };
+  //   }
+
+  switch (action) {
+    case 'UP': {
+      return {
+        countNum: state.countNum + 1,
+      };
+    }
+
+    case 'DOWN': {
+      return {
+        countNum: state.countNum - 1,
+      };
+    }
+    case 'RESET':
+      return { countNum: 0 };
+  }
+
+  return state;
+}
 
 export default function Counter() {
-  const [state, setState] = useState({ value: 0 });
+  const [state, dispatch] = useReducer(counterReducer, initCounterValue);
+  //   const [state, setState] = useState({ countNum: 0 });
   const [cred, setCred] = useState({
     email: '',
     pass: 'secret',
     town: 'Kaunas',
   });
-  console.log('state ===', state);
+  //   console.log('state ===', state);
 
   function goUp() {
-    setState({ value: state.value + 1 });
+    dispatch('UP');
+    // setState({ countNum: state.countNum + 1 });
     // setCred({ email: 'james@bond.com', pass: cred.pass });
     // setCred({ ...cred, email: 'james@bond.com' });
   }
 
   function goDown() {
-    setState({ value: state.value - 1 });
+    dispatch('DOWN');
+    // setState({ countNum: state.countNum - 1 });
   }
-
-  console.log('cred ===', cred);
+  function reset() {
+    dispatch('RESET');
+  }
+  //   console.log('cred ===', cred);
   return (
     <div className='card'>
       <h2>Counter</h2>
-      <p className='counter'>{state.value}</p>
+      <p className='counter'>{state.countNum}</p>
       <div>
         <button onClick={goUp}>UP</button>
         <button onClick={goDown}>DOWN</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={reset}>Up by 10</button>
       </div>
     </div>
   );
