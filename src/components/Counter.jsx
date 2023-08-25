2610a172605c06c016169443fbc2fc170da58c62
Simonas Janulaitis <136367934+SimasCode@users.noreply.github.com>
2023-08-25 10:36:30 +0300

    state object counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,28 +1,29 @@
 import { useState } from 'react';
 
 export default function Counter() {
-  const [counter, setCounter] = useState(0);
+  const [state, setState] = useState({ value: 0 });
   const [cred, setCred] = useState({
     email: '',
     pass: 'secret',
     town: 'Kaunas',
   });
+  console.log('state ===', state);
 
   function goUp() {
-    setCounter(counter + 1);
+    setState({ value: state.value + 1 });
     // setCred({ email: 'james@bond.com', pass: cred.pass });
-    setCred({ ...cred, email: 'james@bond.com' });
+    // setCred({ ...cred, email: 'james@bond.com' });
   }
 
   function goDown() {
-    setCounter(counter - 1);
+    setState({ value: state.value - 1 });
   }
 
   console.log('cred ===', cred);
   return (
     <div className='card'>
       <h2>Counter</h2>
-      <p className='counter'>{counter}</p>
+      <p className='counter'>{state.value}</p>
       <div>
         <button onClick={goUp}>UP</button>
         <button onClick={goDown}>DOWN</button>
